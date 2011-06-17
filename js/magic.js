9dcb374d2befc4c4176746b6d81489d0050e2ab0
Pepijn de Vos <pepijndevos@gmail.com>
2011-06-17 13:35:55 +0200

    search functionality

--- js/magic.js
+++ js/magic.js
@@ -1,5 +1,27 @@
 var timeout = false;
 
+function search(data) {
+	var results = document.getElementById("searchresults");
+	results.innerHTML = "";
+	if(data == false) {
+		var li = document.createElement('li');
+		var text = document.createTextNode("Sad Tapir, nothing found.");
+		li.appendChild(text);
+		results.appendChild(li);	
+	}
+
+	for(idx in data) {
+		var li = document.createElement('li');
+		var a = document.createElement('a');
+		var text = document.createTextNode(data[idx]['title']);
+		a.href = data[idx]['link'];
+		li.appendChild(a);
+		a.appendChild(text);
+		results.appendChild(li);
+
+	}
+}
+
 function run() {
 
 	document.getElementById("chain").onclick = function() {
@@ -15,8 +37,22 @@ function run() {
 	image  = new Image();
 	image.src = "/style/wheel.gif";
 	image.onload = function() {
-		canvas.translate(-200, 400)
-		setInterval("canvas.rotate(0.005); canvas.drawImage(image, -400, -400);", 100)
+		canvas.translate(-200, 400);
+		setInterval("canvas.rotate(0.005); canvas.drawImage(image, -400, -400);", 100);
+	}
+
+	document.getElementById("search").onsubmit = function(form) {
+		var url = this.action + '?';
+		var params = [
+			'token=' + this[0]['value'],
+			'callback=' + this[1]['value'],
+			'query=' + this[2]['value'],
+		];
+		url = url + params.join('&');
+		var script = document.createElement('script');
+		script.src = url;
+		this.appendChild(script);
+		return false;
 	}
 }
 
