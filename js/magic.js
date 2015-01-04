f0ce36b4d0fffc2eeb4f9ffce994e5634a79d5db
Pepijn de Vos <pepijndevos@gmail.com>
2015-01-04 11:23:30 +0000

    css wheel

--- js/magic.js
+++ js/magic.js
@@ -24,20 +24,6 @@ function search(data) {
 
 function run() {
 
-	document.getElementById("chain").onclick = function() {
-		if(!timeout) {
-			timeout = window.setInterval("window.scrollBy(0,1);", 50);
-		} else {
-			window.clearInterval(timeout);
-			timeout = false;
-		}
-	}
-
-	canvas = document.getElementById("wheel").getContext("2d");
-	image = document.getElementById("wheel-img");
-	canvas.translate(-200, 400);
-	setInterval("canvas.rotate(0.005); canvas.drawImage(image, -400, -400);", 100);
-
 	document.getElementById("search").onsubmit = function(form) {
 		var url = this.action + '?';
 		var params = [
